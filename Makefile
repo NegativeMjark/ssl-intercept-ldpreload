ea933edef98b3df318cd17610e8d5ffdc37d459f
Mark Haines <mark.haines@openmarket.com>
2014-12-11 17:14:43 +0000

    Add environment variable for controlling the directory where the plaintext is written to

--- Makefile
+++ Makefile
@@ -1,5 +1,5 @@
-default: libinterceptssl.so
-
-libinterceptssl.so:
+libinterceptssl.so: interceptssl.c
 	gcc -shared interceptssl.c -o libinterceptssl.so \
 		-Wl,--no-as-needed -ldl -lssl -D_GNU_SOURCE -fpic
+
+default: libinterceptssl.so
--- interceptssl.c
+++ interceptssl.c
@@ -4,11 +4,21 @@
 
 const int MAX_NAME = 1024;
 
+static char *OUTPUT_DIR = "";
+
+static void set_output_dir() {
+    char *output_dir = getenv("INTERCEPT_SSL_OUTPUT_DIR");
+    if (output_dir) {
+        OUTPUT_DIR = output_dir;
+    }
+}
+
 static int (*next_SSL_read)(SSL *ssl, void *buf, int num) = NULL;
 
 int SSL_read(SSL *ssl, void *buf, int num) {
     if (next_SSL_read == NULL) {
         next_SSL_read = dlsym(RTLD_NEXT, "SSL_read");
+        set_output_dir();
     }
 
     int ret = next_SSL_read(ssl, buf, num);
@@ -16,7 +26,8 @@ int SSL_read(SSL *ssl, void *buf, int num) {
     if (ret > 0) {
         char filename[MAX_NAME];
         FILE *file;
-        snprintf(filename, sizeof(filename), "SSL_read.%p", ssl);
+        snprintf(filename, sizeof(filename),
+                 "%s/SSL_read.%p", OUTPUT_DIR, ssl);
         file = fopen(filename, "a");
         if (file) {
             fwrite(buf, 1, ret, file);
@@ -31,6 +42,7 @@ int (*next_SSL_write)(SSL *ssl, const void *buf, int num) = NULL;
 int SSL_write(SSL *ssl, const void *buf, int num) {
     if (next_SSL_write == NULL) {
         next_SSL_write = dlsym(RTLD_NEXT, "SSL_write");
+        set_output_dir();
     }
 
     int ret = next_SSL_write(ssl, buf, num);
@@ -38,7 +50,8 @@ int SSL_write(SSL *ssl, const void *buf, int num) {
     if (ret > 0) {
         char filename[MAX_NAME];
         FILE *file;
-        snprintf(filename, sizeof(filename), "SSL_write.%p", ssl);
+        snprintf(filename, sizeof(filename),
+                 "%s/SSL_write.%p", OUTPUT_DIR, ssl);
         file = fopen(filename, "a");
         if (file) {
             fwrite(buf, 1, ret, file);
